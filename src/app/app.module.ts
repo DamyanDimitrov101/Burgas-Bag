c48911bc9fbad05b1b6869add9ec83f6d8e3ad64
Damyan Dimitrov <damqn_96@abv.bg>
2023-08-15 03:48:29 +0300

    Initial set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FirebaseTSApp } from "firebasets/firebasetsApp/firebaseTSApp";
+import { environment } from "src/environments/environment";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(){
+    
+  }
+ }
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+
+export const environment = {
+    production: false,
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+    apiKey: "AIzaSyCFr5gyPv9XI8i4jSYQpyUQJEStEE-wOH8",
+    authDomain: "burgas-bag.firebaseapp.com",
+    projectId: "burgas-bag",
+    storageBucket: "burgas-bag.appspot.com",
+    messagingSenderId: "737568333372",
+    appId: "1:737568333372:web:77867d8af27a1828e22d39",
+    measurementId: "G-2G3ZF2JQSK"
+  }
+}
