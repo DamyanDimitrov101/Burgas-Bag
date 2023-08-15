489776d732917c10b89abe9cbc77dac2e61e69b9
Damyan Dimitrov <damqn_96@abv.bg>
2023-08-15 07:06:36 +0300

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FirebaseTSApp } from "firebasets/firebasetsApp/firebaseTSApp";
 import { environment } from "src/environments/environment";
+import { HomeComponent } from './pages/home/home.component';
+
+import { MatButtonModule } from "@angular/material/button"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor(){
-    
+    FirebaseTSApp.init(environment.firebaseConfig)
   }
  }
